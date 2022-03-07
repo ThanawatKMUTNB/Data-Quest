c6b783498759c4197e9eabc377703e2afd35b165
tatazakup <s6301012620022@email.kmutnb.ac.th>
2022-03-08 00:36:13 +0700

    add html image

--- start.py
+++ start.py
@@ -1,6 +1,9 @@
+from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 import csv
+from IPython.display import HTML
+from soupsieve import escape
 # print('kiki')
 
 url = "https://myanimelist.net/topanime.php"
@@ -26,14 +29,28 @@ table = soup.find("table", attrs={ "class" : "top-ranking-table" })
 
 rows = table.find_all('tr')
 for row in rows:
-    # print(row)
+    # print("\n\n",row)
+    # print("\n\n",row.find_all('img'))
     cols = row.find_all('td')
     cols = [ele.text.strip() for ele in cols]
+    img = row.find_all('img')
+    # print(cols)
+    if len(img) == 0:
+        cols.append("Image")
+    if len(img) > 0:
+        src = img[0]["data-src"]
+        imghtml = f'<img src ="{src}"/>'
+        # print(type(HTML(imghtml)))
+        cols.append(src)
+        # cols[-1] = HTML(cols[-1])
+        # print("\n\n",img[0]["data-src"])
     data.append([ele for ele in cols if ele]) # Get rid of empty values
     
 # print(data)
+# print(type(data[-1]))
 
 with open('animeRankTable.csv', 'w') as f:
     # using csv.writer method from CSV package
     write = csv.writer(f)
     write.writerows(data)
+f.close()
